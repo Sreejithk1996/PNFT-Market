9d3fa06ee7722906598d85127a3bfe7a2f7189d3
Sreejithk1996 <sreekumar1996@gmail.com>
2024-05-14 17:34:06 +0530

    updated

--- src/components/todo/todocard/ToDoCard.jsx
+++ src/components/todo/todocard/ToDoCard.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
 import List from '../list/List'
 import axios from 'axios'
+import { useEffect } from 'react'
 
 
 function ToDoCard() {
+    const [data,setData] = useState([]);
+    useEffect(()=>{
+        axios.get('https://663a5a501ae792804bef03fe.mockapi.io/todo/todo')
+        .then(res=>{
+            setData(res.data)
+        }).catch(err=>{
+            console.log(err)
+    })
+    },[])
+
+
   return (
        <div className='bg-[#ECEEF4] h-[300px] p-3 rounded'>
             <div className='py-4'>
                 <p className='font-semibold text-base'>TO DO</p>
             </div>
-            <List />
+            <List data={data} />
         </div>
   )
 }
