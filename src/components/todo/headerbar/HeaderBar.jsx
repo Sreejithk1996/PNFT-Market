c44432a99a1ca56d68917d29e7435449ad21244e
Sreejithk1996 <sreekumar1996@gmail.com>
2024-05-17 10:21:22 +0530

    updated

--- src/components/todo/headerbar/HeaderBar.jsx
+++ src/components/todo/headerbar/HeaderBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import titlelogo from "../../../assets/images/Logo-title.png";
 import logouticon from "../../../assets/images/XMLID_2_.svg";
-import Avatar from '../../../assets/images/Avatar.png'
+import Avatar from '../../../assets/images/Avatar1.png'
 
 function HeaderBar() {
 
@@ -17,7 +17,7 @@ function HeaderBar() {
             </div>
             <div className='flex flex-row-reverse items-center gap-3'>
                 <button onClick={handleClick}><img className='transition delay-100 hover:rotate-[-90deg] hover:transition delay-100 sm:mx-[20px]' src={logouticon} alt="" /></button>
-                <img className='w-[50px] border-[#3cc92c] border-4 rounded-[50%]' src={Avatar} alt="" />
+                <img className='w-[50px] border-[#65de1b] border-4 rounded-[50%]' src={Avatar} alt="" />
             </div>
         </>
     )
